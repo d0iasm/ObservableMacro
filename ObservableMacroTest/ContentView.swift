9d351b3b111ced7ceb509daf25421ccd3d3f1a5c
Asami Doi <asamidoi@chromium.org>
2023-06-21 11:50:09 +0200

    Initial Commit

---
+++ ObservableMacroTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ObservableMacroTest
+//
+//  Created by Asami Doi on 21/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ObservableMacroTest/ObservableMacroTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableMacroTestApp.swift
+//  ObservableMacroTest
+//
+//  Created by Asami Doi on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ObservableMacroTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
